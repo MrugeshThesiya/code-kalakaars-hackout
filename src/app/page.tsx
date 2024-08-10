0ce1821ff4dcd49d32b32db763dc886c3bfa766b
Ven-cy <160127240+Ven-cy@users.noreply.github.com>
2024-08-10 17:22:44 +0530

    map added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
+import React from "react";
+
 export default function Home() {
-  
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl text-center">Hello World</h1>
+      <h1 className="text-2xl text-center"><img src="https://iili.io/dlMHYYX.jpg" alt="" /></h1>
     </div>
   );
 }
